cf786cf69f165a93755a0a033c0076c9b7d0151a
祥子 <2697301927@qq.com>
2020-06-28 11:39:06 +0800

    修改

--- vue.config.js
+++ vue.config.js
@@ -17,6 +17,7 @@ module.exports = {
         return args
       })
     })
+    
     // 开发模式
     config.when(process.env.NODE_ENV === 'development', config => {
       config.entry('app').clear().add('./src/main-dev.js')
